c151b3ceb1c9c002f0c133cf967aac95cf9ed045
Jerry Zheng <u6579712@anu.edu.au>
2020-05-19 19:21:02 +0800

    pop dialog to create new ve

--- app/src/main/java/com/example/votingapp/VotingEdit.java
+++ app/src/main/java/com/example/votingapp/VotingEdit.java
@@ -1,9 +1,18 @@
 package com.example.votingapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -21,14 +30,14 @@ public class VotingEdit extends AppCompatActivity {
         mAddButton = findViewById(R.id.fab_add);
         mChoiceButton = findViewById(R.id.fab_choice);
         mTextButton = findViewById(R.id.fab_text);
-
+        //the add button with itself rotation
         mAddButton.setOnClickListener(new View.OnClickListener() {
             boolean isRotate = false;
             @Override
             public void onClick(View v) {
                 isRotate = expandFAB(v, isRotate);
             }
-
+//            once click, rotate addButton 135 degree or reset(click twice).
             private boolean expandFAB(View v, boolean isRotate) {
                 if (!isRotate) {
                     v.animate().rotation(135);
@@ -42,7 +51,48 @@ public class VotingEdit extends AppCompatActivity {
                 return !isRotate;
             }
         });
+//        the choice button listener
+        mChoiceButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+//            once click the choice button, s.t.
+            public void onClick(View v){
 
+            }
+        });
+//        text choice button listener, when clicked to create a new voting confirm dialog
+        mTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popCreateDialog();
+            }
+//            dialog to create a new voting event with title set
+            private void popCreateDialog(){
+                final EditText editText = new EditText(VotingEdit.this);
+                final AlertDialog.Builder newVotingEventDialog = new AlertDialog.Builder(VotingEdit.this);
+                final View dialogView = LayoutInflater.from(VotingEdit.this).inflate(R.layout.create_dialog,null);
+                newVotingEventDialog.setTitle("Create New Voting Event").setView(editText);
+                newVotingEventDialog.setMessage("give your voting event a name!");
 
+//                if press create, record the title of voting event, go through next page
+                newVotingEventDialog.setPositiveButton("create!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                      extract the title of voting event
+                        EditText edit_text = (EditText) dialogView.findViewById(R.id.edit_text);
+                        String titleOfVotingEvent = edit_text.getText().toString();
+                        Log.d("title test print:",titleOfVotingEvent);
+                        Toast.makeText(VotingEdit.this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+//                if press cancel , quit the dialog
+                newVotingEventDialog.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                newVotingEventDialog.show();
+            }
+        });
     }
 }
