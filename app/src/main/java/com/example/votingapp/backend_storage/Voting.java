da1336da88b526288ed81feecafe733e3d77aa32
Jerry Zheng <u6579712@anu.edu.au>
2020-05-21 00:15:14 +0800

    voting class & data structure done

--- app/src/main/java/com/example/votingapp/backend_storage/Voting.java
+++ app/src/main/java/com/example/votingapp/backend_storage/Voting.java
@@ -3,31 +3,42 @@ package com.example.votingapp.backend_storage;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Voting {
-
+abstract class Voting{
 //    creator uid of this voting
-    private String creatorUid;
+    String creatorUid;
 //    voting due date
-    private Date deadline;
+    Date deadline;
+    abstract String getCreatorUid();
+    abstract Date getDeadline();
 
-//    main data structure of voting
-    private ArrayList<Question> questions;
-
-    Voting(String creatorUid, Date deadline,ArrayList<Question> questions){
-        this.creatorUid = creatorUid;
-        this.deadline = deadline;
-        this.questions = questions;
-    }
-//    return creator uid
-    public String getCreatorUid(){
-        return this.creatorUid;
-    }
-//    return deadline
-    public Date getDeadline(){
-        return this.deadline;
-    }
-//    return questions in this voting
-    public ArrayList<Question> getQuestions(){
-        return this.questions;
-    }
 }
+
+//public class Voting {
+//
+////    creator uid of this voting
+//    private String creatorUid;
+////    voting due date
+//    private Date deadline;
+//
+////    main data structure of voting
+//    private ArrayList<Question> questions;
+//
+//    Voting(String creatorUid, Date deadline,ArrayList<Question> questions){
+//        this.creatorUid = creatorUid;
+//        this.deadline = deadline;
+//        this.questions = questions;
+//    }
+////    return creator uid
+//    public String getCreatorUid(){
+//        return this.creatorUid;
+//    }
+////    return deadline
+//    public Date getDeadline(){
+//        return this.deadline;
+//    }
+////    return questions in this voting
+//    public ArrayList<Question> getQuestions(){
+//        return this.questions;
+//    }
+//}
+
---
+++ app/src/main/java/com/example/votingapp/backend_storage/VotingResults.java
@@ -0,0 +1,32 @@
+package com.example.votingapp.backend_storage;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class VotingResults extends Voting{
+//    for creator only main structure
+    private ArrayList<QuestionStatistics> questionStatistics = new ArrayList<>();
+//      the result of each question has been stored in questionStatistics class.
+    VotingResults(String creatorUid, Date deadline, ArrayList<QuestionStatistics> questions) {
+        super.creatorUid = creatorUid;
+        super.deadline = deadline;
+        this.questionStatistics =questions;
+    }
+
+//    public void addAnswers()
+
+//    if you want the whole data structure
+    public ArrayList<QuestionStatistics> getQuestionStatistics(){
+        return this.questionStatistics;
+    }
+
+    @Override
+    public String getCreatorUid() {
+        return this.creatorUid;
+    }
+
+    @Override
+    public Date getDeadline() {
+        return this.deadline;
+    }
+}
