a1a44fd08fdab111cc25a416f66c6363880df367
Jarvis Huang <u5885823@anu.edu.au>
2020-05-22 20:53:12 +1000

    Multiple Choice Question editing activity

--- app/src/main/java/com/example/votingapp/MultiChoiceQuestion.java
+++ app/src/main/java/com/example/votingapp/MultiChoiceQuestion.java
@@ -12,31 +12,65 @@ import java.util.ArrayList;
 
 public class MultiChoiceQuestion extends AppCompatActivity {
     private Button saveButton;
+    private Button cancelButton;
     private String textQuestion;
     private EditText questionFiled;
-    //private String choice1;
-    //private String choice2;
-    private EditText choice1_edittext;
-    private EditText choice2_edittext;
+    private final int choiceSize=8;
+    private final String choiceIDFormat = "editText_choice";
     private ArrayList<String> choices = new ArrayList<>();
+    private EditText tempChoiceID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_multi_choice_question);
+        setContentView(R.layout.activity_multiple_choice_question);
         questionFiled = findViewById(R.id.editText_question);
+        saveButton = findViewById(R.id.multi_button_save);
+        cancelButton = findViewById(R.id.multi_button_cancel);
 
-        saveButton = findViewById(R.id.save_button_multichoice_);
+
+
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 textQuestion = questionFiled.getText().toString();
 
+
+                tempChoiceID=findViewById(R.id.editText_choice0);
+                choices.add(tempChoiceID.getText().toString());
+                tempChoiceID=findViewById(R.id.editText_choice1);
+                choices.add(tempChoiceID.getText().toString());
+                tempChoiceID=findViewById(R.id.editText_choice2);
+                choices.add(tempChoiceID.getText().toString());
+                tempChoiceID=findViewById(R.id.editText_choice3);
+                choices.add(tempChoiceID.getText().toString());
+                tempChoiceID=findViewById(R.id.editText_choice4);
+                choices.add(tempChoiceID.getText().toString());
+                tempChoiceID=findViewById(R.id.editText_choice5);
+                choices.add(tempChoiceID.getText().toString());
+                tempChoiceID=findViewById(R.id.editText_choice6);
+                choices.add(tempChoiceID.getText().toString());
+                tempChoiceID=findViewById(R.id.editText_choice7);
+                choices.add(tempChoiceID.getText().toString());
+
+
                 Intent replyIntent = new Intent();
                 replyIntent.putExtra("key", textQuestion);
                 replyIntent.putStringArrayListExtra("key_choices", choices);
+
                 setResult(RESULT_OK, replyIntent);
                 finish();
             }
         });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+
+
     }
 }
