603cfebe3e523b8b33ee4b4389972081a64beaba
Jarvis Huang <u5885823@anu.edu.au>
2020-05-22 13:26:09 +1000

    Multi_choice editing layout

--- app/src/main/java/com/example/votingapp/MultiChoiceQuestion.java
+++ app/src/main/java/com/example/votingapp/MultiChoiceQuestion.java
@@ -14,8 +14,8 @@ public class MultiChoiceQuestion extends AppCompatActivity {
     private Button saveButton;
     private String textQuestion;
     private EditText questionFiled;
-    private String choice1;
-    private String choice2;
+    //private String choice1;
+    //private String choice2;
     private EditText choice1_edittext;
     private EditText choice2_edittext;
     private ArrayList<String> choices = new ArrayList<>();
@@ -25,7 +25,7 @@ public class MultiChoiceQuestion extends AppCompatActivity {
         setContentView(R.layout.activity_multi_choice_question);
         questionFiled = findViewById(R.id.editText_question);
 
-        saveButton = findViewById(R.id.save_button_multichoice);
+        saveButton = findViewById(R.id.save_button_multichoice_);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
